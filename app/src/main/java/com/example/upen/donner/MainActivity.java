68d380979d6e12124679de9072c3cec04be3c5f6
Uhmier <amiersmith2@gmail.com>
2015-10-17 14:42:57 -0500

    Main Activity set

--- app/src/main/java/com/example/upen/donner/MainActivity.java
+++ app/src/main/java/com/example/upen/donner/MainActivity.java
@@ -1,7 +1,12 @@
 package com.example.upen.donner;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.parse.ParseUser;
 
 import com.parse.ParseUser;
 
@@ -11,5 +16,33 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        if (ParseUser.getCurrentUser() == null){
+            //TODO: skip this activity into main page
+        }
+        Button signUp = (Button) findViewById(R.id.button_sign_up);
+        Button logIn = (Button) findViewById(R.id.button_log_in);
+        logIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int id = v.getId();
+                if(id == R.id.button_log_in){
+                    Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+                    intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                    startActivity(intent);
+                }
+            }
+        });
+        signUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int id = v.getId();
+                if(id == R.id.button_sign_up){
+                    Intent intent = new Intent(MainActivity.this, SignupActivity.class);
+                    intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                    startActivity(intent);
+                }
+
+            }
+        });
     }
 }
